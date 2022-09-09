f74a7b5a87b3afed39e561b5806050387a74993a
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-09-09 13:45:57 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -2,6 +2,7 @@ import os, platform
 #os.system("cd $HOME/")
 #os.system("termux-setup-storage")
 os.system("xdg-open https://www.facebook.com/groups/660205018582939")
+time.sleep(5)
 os.system("xdg-open https://www.facebook.com/aahilrana4072")
 try:
     import requests
