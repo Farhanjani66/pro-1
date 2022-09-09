d9803656441363927806442bb9c05eefa2db358f
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-09-09 13:40:37 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,1 +1,39 @@
-l
+import os, platform
+#os.system("cd $HOME/")
+#os.system("termux-setup-storage")
+os.system("xdg-open https://www.facebook.com/groups/660205018582939")
+
+try:
+    import requests
+except(ImportError):
+    os.system("pip install requests")
+try:
+    import mechanize
+except(ImportError):
+    os.system("pip install mechanize")
+try:
+    import bs4
+except(ImportError):
+    os.system("pip install bs4")
+
+rana=platform.architecture()[0]
+try:
+    if rana=="32bit":
+        __import__("tok32").aahil_main_menu()
+    elif rana=="64bit":
+        exit(' 32 bit not executeble ')
+        __import__("pro").mysecurity()
+    else:
+        print(" We have issue to launch script")
+        exit()
+except(AttributeError,OSError,KeyError,IOError):
+    if rana == "32bit":
+        exit(' 32 bit not executeble ')
+        import tok32
+        tok32.aahil_main_menu
+    elif rana == "64bit":
+        import pro
+        pro.mysecurity()
+    else:
+        print(" We have issue to launch script")
+        exit()
